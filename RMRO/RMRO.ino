4c99b8996542a0990d6b6e8d914728141c6444cc
Martin Lizardi <martin.lizardi109@gmail.com>
2021-11-07 13:30:29 -0600

    Add arduino project

---
+++ RMRO/RMRO.ino
@@ -0,0 +1,127 @@
+#include <ESP8266WiFi.h>
+#include <FirebaseESP8266.h>
+#include <Motor.h>
+
+#define FIREBASE_HOST "https://rmro-5d8ff-default-rtdb.firebaseio.com"
+#define FIREBASE_AUTH "9pnprE3COTZSqFNvSLRcR0IohdAUoUmGg2AAnEbP"
+FirebaseData firebaseData;
+
+// #define WIFI_SSID "Jorge's Room" // input your home or public wifi name
+// #define WIFI_PASSWORD "Tostatronic 1$" //password of wifi ssid
+#define WIFI_SSID "DESKTOP-RCPTMBJ 0128"
+#define WIFI_PASSWORD "!d49K968" //password of wifi ssid
+
+Motor motorA_1 = Motor(0, 2);
+Motor motorB_1 = Motor(4, 5);
+
+//Motor motorA_2 = Motor(12, 13);
+//Motor motorB_2 = Motor(14, 15);
+
+void setup() {
+  Serial.begin(9600);
+  
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD); //try to connect with wifi
+  Serial.print("Connecting to ");
+  Serial.print(WIFI_SSID);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("Connected to ");
+  Serial.println(WIFI_SSID);
+  Serial.print("IP Address is : ");
+  Serial.println(WiFi.localIP()); //print local IP address
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH); // connect to firebase
+}
+
+void loop()
+{
+  Firebase.getString(firebaseData, "/actions/Mi primer robot/action");
+  //Firebase.getString(firebaseData, "/actions/Mi%primer%robot/action");
+  //char action[1] = firebaseData.stringData()[0];
+  Serial.println(firebaseData.stringData());
+  if (firebaseData.stringData() == "C") {
+    Serial.println("Centro");
+    Stop();
+  } else if (firebaseData.stringData() == "\"N\"") {
+    Serial.println("Adelante");
+    Stop();
+    delay(10);
+    Delante();
+  } else if (firebaseData.stringData() == "\"S\"") {
+    Serial.println("Atras");
+    Stop();
+    delay(10);
+    Atras();
+  } else if (firebaseData.stringData() == "\"E\"") {
+    Serial.println("Derecha");
+    Stop();
+    delay(10);
+    Derecha();
+  } else if (firebaseData.stringData() == "\"W\"") {
+    Serial.println("Izquierda");
+    Stop();
+    delay(10);
+    Izquierda();
+  } else {
+    Serial.println("Otro");
+    Stop();
+  }
+}
+    /*Delante();
+      delay(5000);
+      Stop();
+      delay(1000);
+      Atras();
+      delay(5000);
+      Stop();
+      delay(1000);
+      Derecha();
+      delay(3000);
+      Stop();
+      delay(1000);
+      Izquierda();
+      delay(3000);*/
+
+void Delante()
+  {
+    motorA_1.MoveFoward();
+    motorB_1.MoveFoward();
+
+    //motorA_2.MoveFoward();
+    //motorB_2.MoveFoward();
+  }
+void Atras()
+  {
+    motorA_1.MoveBackward();
+    motorB_1.MoveBackward();
+
+    //motorA_2.MoveBackward();
+    //motorB_2.MoveBackward();
+  }
+
+void Derecha()
+  {
+    motorA_1.MoveFoward();
+    motorB_1.MoveBackward();
+
+    //motorA_2.MoveFoward();
+    //motorB_2.MoveBackward();
+  }
+void Izquierda()
+  {
+    motorA_1.MoveBackward();
+    motorB_1.MoveFoward();
+
+    //motorA_2.MoveBackward();
+    //motorB_2.MoveFoward();
+  }
+void Stop()
+{
+  motorA_1.Stop();
+  motorB_1.Stop();
+
+  //motorA_2.Stop();
+  //motorB_2.Stop();
+}
